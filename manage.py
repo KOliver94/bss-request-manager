4c0d228391815a8fa491dd76817a58c029c97e69
Oliver <19346498+KOliver94@users.noreply.github.com>
2019-12-12 00:18:27 +0100

    Added LDAP authentication

--- manage.py
+++ manage.py
@@ -3,6 +3,16 @@
 import os
 import sys
 
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_value(env_variable):
+    try:
+        return os.environ[env_variable]
+    except KeyError:
+        error_msg = 'Set the {} environment variable'.format(env_variable)
+        raise ImproperlyConfigured(error_msg)
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manager.settings')
