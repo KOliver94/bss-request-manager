c6c946db54f85ea76235efa6a076348f9596641e
Oliver <19346498+KOliver94@users.noreply.github.com>
2020-03-10 01:15:49 +0100

    Testing settings  - Coverage support  - JSON response for tests

--- manager/settings.py
+++ manager/settings.py
@@ -180,7 +180,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 # Simple JWT Settings
