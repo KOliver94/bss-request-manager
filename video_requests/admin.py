2d09eb7270d9523ea86d7626f4554b2b9b787c89
Olivér Kecskeméty <oliver.kecskemety@ericsson.com>
2020-03-13 16:57:21 +0100

    Use explicit import

--- video_requests/admin.py
+++ video_requests/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Comment, CrewMember, Video, Request, Rating
+from video_requests.models import Comment, CrewMember, Video, Request, Rating
 
 admin.site.register([Request, Video, Comment, CrewMember, Rating])
