787ab78dac2f570696ce88bf86004b3bf5f6b016
Oliver <19346498+KOliver94@users.noreply.github.com>
2020-03-16 17:27:20 +0100

    Removed trailing slash from default API

--- api/v1/requests/urls.py
+++ api/v1/requests/urls.py
@@ -5,12 +5,12 @@ from api.v1.requests.views import RequestDefaultListCreateView, RequestDefaultDe
     RatingDefaultListCreateView, RatingDefaultDetailView
 
 urlpatterns = [
-    path('', RequestDefaultListCreateView.as_view()),
-    path('<int:pk>', RequestDefaultDetailView.as_view()),
-    path('<int:requestId>/comments', CommentDefaultListCreateView.as_view()),
-    path('<int:requestId>/comments/<int:pk>', CommentDefaultDetailView.as_view()),
-    path('<int:requestId>/videos', VideoDefaultListView.as_view()),
-    path('<int:requestId>/videos/<int:pk>', VideoDefaultDetailView.as_view()),
-    path('<int:requestId>/videos/<int:videoId>/ratings', RatingDefaultListCreateView.as_view()),
-    path('<int:requestId>/videos/<int:videoId>/ratings/<int:pk>', RatingDefaultDetailView.as_view()),
+    path('requests', RequestDefaultListCreateView.as_view()),
+    path('requests/<int:pk>', RequestDefaultDetailView.as_view()),
+    path('requests/<int:requestId>/comments', CommentDefaultListCreateView.as_view()),
+    path('requests/<int:requestId>/comments/<int:pk>', CommentDefaultDetailView.as_view()),
+    path('requests/<int:requestId>/videos', VideoDefaultListView.as_view()),
+    path('requests/<int:requestId>/videos/<int:pk>', VideoDefaultDetailView.as_view()),
+    path('requests/<int:requestId>/videos/<int:videoId>/ratings', RatingDefaultListCreateView.as_view()),
+    path('requests/<int:requestId>/videos/<int:videoId>/ratings/<int:pk>', RatingDefaultDetailView.as_view()),
 ]
