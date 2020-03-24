2e058a21a74cca2172a57eebb482dddecb96def0
Oliver <19346498+KOliver94@users.noreply.github.com>
2020-03-25 00:21:14 +0100

    Save anonymous requester's data to additional_data

--- api/v1/requests/serializers.py
+++ api/v1/requests/serializers.py
@@ -121,17 +121,28 @@ class RequestAnonymousSerializer(serializers.ModelSerializer):
         phone_number = validated_data.pop('requester_mobile') if 'requester_mobile' in validated_data else None
 
         if User.objects.filter(email__iexact=user.email).exists():
-            return User.objects.get(email__iexact=user.email)
+            # If user with given e-mail address already exist set it as requester but save the provided data.
+            # The user's data will be not overwritten so useful to check phone number or anything else.
+            additional_data = {
+                'requester': {
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'phone_number': phone_number.as_e164
+                }}
+            return User.objects.get(email__iexact=user.email), additional_data
         else:
             user.save()
             user.userprofile.phone_number = phone_number
             user.save()
-            return user
+            return user, None
 
     def create(self, validated_data):
         comment_text = validated_data.pop('comment_text') if 'comment_text' in validated_data else None
-        validated_data['requester'] = self.create_user(validated_data)
+        validated_data['requester'], additional_data = self.create_user(validated_data)
         request = super(RequestAnonymousSerializer, self).create(validated_data)
+        if additional_data:
+            request.additional_data = additional_data
         if comment_text:
             request.comments.add(create_comment(comment_text, request))
+        request.save()
         return request
