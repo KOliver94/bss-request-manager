2ecd01b6f2a164a886c8eabfa655d808d2585f84
Olivér Kecskeméty <19346498+KOliver94@users.noreply.github.com>
2020-04-04 22:21:11 +0200

    Scheduled task to update request status

---
+++ video_requests/management/commands/update_request_status.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+from django.core.management import BaseCommand
+
+from video_requests.models import Request
+from video_requests.utilities import update_request_status
+
+
+class Command(BaseCommand):
+    help = 'Update status of requests which should be recorded by this time'
+
+    def handle(self, *args, **options):
+        to_update = Request.objects.filter(status=2, date__lt=date.today())
+        for request in to_update:
+            update_request_status(request)
+        self.stdout.write(self.style.SUCCESS(f'{to_update.count()} requests was checked for valid status.'))
