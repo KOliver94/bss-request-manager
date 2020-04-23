56ff48c00d85aafd083a8db4c7157ebfb2a7f49a
Olivér Kecskeméty <19346498+KOliver94@users.noreply.github.com>
2020-04-23 14:16:38 +0200

    Fix daily reminder email

--- backend/video_requests/management/commands/email_daily_reminders.py
+++ backend/video_requests/management/commands/email_daily_reminders.py
@@ -1,4 +1,4 @@
-from datetime import date
+import datetime
 
 from django.core.management import BaseCommand
 
@@ -10,7 +10,9 @@ class Command(BaseCommand):
     help = "Send reminder to crew members of today's requests"
 
     def handle(self, *args, **options):
-        requests = Request.objects.filter(status__range=[1, 2], date=date.today())
+        today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+        today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+        requests = Request.objects.filter(status__range=[1, 2], start_datetime__range=[today_min, today_max])
 
         total_sent = 0
 
