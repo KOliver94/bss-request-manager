45f233425ddb2ac1cff793ff6618c1465096aec0
Olivér Kecskeméty <19346498+KOliver94@users.noreply.github.com>
2020-05-01 23:30:03 +0200

    Changed pagination to contain total page number

--- backend/manager/settings.py
+++ backend/manager/settings.py
@@ -306,7 +306,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'common.pagination.ExtendedPagination',
     'PAGE_SIZE': 100,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
