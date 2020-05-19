898e9f4e99aaba7eadf6ddaf1f88ad640ff0ef70
Olivér Kecskeméty <19346498+KOliver94@users.noreply.github.com>
2020-05-19 02:23:43 +0200

    Fixed wrong date check

--- backend/video_requests/management/commands/update_request_status.py
+++ backend/video_requests/management/commands/update_request_status.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 
 from django.core.management import BaseCommand
 
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     help = 'Update status of requests which should be recorded by this time'
 
     def handle(self, *args, **options):
-        to_update = Request.objects.filter(status=2, date__lt=date.today())
+        to_update = Request.objects.filter(status=2, end_datetime__lte=datetime.now())
         for request in to_update:
             update_request_status(request)
         self.stdout.write(self.style.SUCCESS(f'{to_update.count()} requests was checked for valid status.'))
