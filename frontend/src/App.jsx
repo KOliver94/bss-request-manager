c102845df9e9b07f9a848f18e60a08ff7425d015
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2020-12-06 01:05:42 +0100

    Use the same colors everywhere

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import {
+  primaryColor,
+  roseColor as secondaryColor,
+  dangerColor as errorColor,
+  warningColor,
+  infoColor,
+  successColor,
+  grayColor,
+} from 'assets/jss/material-kit-react';
 
 import AuthenticatedRoute from './components/AuthenticatedRoute';
 import AdminRoute from './components/AdminRoute';
@@ -13,64 +23,92 @@ import RequestDetailPage from './views/RequestDetailPage/RequestDetailPage';
 
 import 'assets/scss/material-kit-react.scss';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: primaryColor,
+    },
+    secondary: {
+      main: secondaryColor,
+    },
+    error: {
+      main: errorColor,
+    },
+    warning: {
+      main: warningColor,
+    },
+    info: {
+      main: infoColor,
+    },
+    success: {
+      main: successColor,
+    },
+    grey: {
+      main: grayColor,
+    },
+  },
+});
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(
     !!localStorage.getItem('access_token')
   );
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage
-            isAuthenticated={isAuthenticated}
-            setIsAuthenticated={setIsAuthenticated}
-          />
-        </Route>
-        <Route path="/login">
-          <LoginPage
-            isAuthenticated={isAuthenticated}
-            setIsAuthenticated={setIsAuthenticated}
-          />
-        </Route>
-        <Route exact path="/new-request">
-          <RequestCreatorPage
-            isAuthenticated={isAuthenticated}
-            setIsAuthenticated={setIsAuthenticated}
-          />
-        </Route>
-        <AuthenticatedRoute exact path="/my-requests">
-          <MyRequestsPage
-            isAuthenticated={isAuthenticated}
-            setIsAuthenticated={setIsAuthenticated}
-          />
-        </AuthenticatedRoute>
-        <AuthenticatedRoute exact path="/my-requests/:id">
-          <RequestDetailPage
-            isAuthenticated={isAuthenticated}
-            setIsAuthenticated={setIsAuthenticated}
-          />
-        </AuthenticatedRoute>
-        <AuthenticatedRoute exact path="/admin/requests">
-          <AdminRoute>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage
+              isAuthenticated={isAuthenticated}
+              setIsAuthenticated={setIsAuthenticated}
+            />
+          </Route>
+          <Route path="/login">
+            <LoginPage
+              isAuthenticated={isAuthenticated}
+              setIsAuthenticated={setIsAuthenticated}
+            />
+          </Route>
+          <Route exact path="/new-request">
+            <RequestCreatorPage
+              isAuthenticated={isAuthenticated}
+              setIsAuthenticated={setIsAuthenticated}
+            />
+          </Route>
+          <AuthenticatedRoute exact path="/my-requests">
             <MyRequestsPage
               isAuthenticated={isAuthenticated}
               setIsAuthenticated={setIsAuthenticated}
-              isAdmin
             />
-          </AdminRoute>
-        </AuthenticatedRoute>
-        <AuthenticatedRoute exact path="/admin/requests/:id">
-          <AdminRoute>
+          </AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/my-requests/:id">
             <RequestDetailPage
               isAuthenticated={isAuthenticated}
               setIsAuthenticated={setIsAuthenticated}
-              isAdmin
             />
-          </AdminRoute>
-        </AuthenticatedRoute>
-        <Route component={PageNotFound} />
-      </Switch>
-    </Router>
+          </AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/admin/requests">
+            <AdminRoute>
+              <MyRequestsPage
+                isAuthenticated={isAuthenticated}
+                setIsAuthenticated={setIsAuthenticated}
+                isAdmin
+              />
+            </AdminRoute>
+          </AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/admin/requests/:id">
+            <AdminRoute>
+              <RequestDetailPage
+                isAuthenticated={isAuthenticated}
+                setIsAuthenticated={setIsAuthenticated}
+                isAdmin
+              />
+            </AdminRoute>
+          </AuthenticatedRoute>
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- frontend/src/assets/jss/material-kit-react.js
+++ frontend/src/assets/jss/material-kit-react.js
@@ -70,11 +70,11 @@ const defaultFont = {
 };
 
 const primaryColor = '#9c27b0';
-const warningColor = '#ff9800';
-const dangerColor = '#f44336';
-const successColor = '#4caf50';
-const infoColor = '#00acc1';
 const roseColor = '#e91e63';
+const dangerColor = '#f44336';
+const warningColor = '#ff9800';
+const infoColor = '#00acc1';
+const successColor = '#4caf50';
 const grayColor = '#999999';
 
 const primaryBoxShadow = {
