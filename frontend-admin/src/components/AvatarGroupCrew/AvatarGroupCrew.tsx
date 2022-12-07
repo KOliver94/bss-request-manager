f52fa418128df811c7fb113af9ed0ef63c4ec52e
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2022-12-07 02:06:03 +0100

    Create Avatar Group for crew members

---
+++ frontend-admin/src/components/AvatarGroupCrew/AvatarGroupCrew.tsx
@@ -0,0 +1,77 @@
+import { forwardRef, Fragment } from 'react';
+
+import { Avatar } from 'primereact/avatar';
+import { AvatarGroup, AvatarGroupProps } from 'primereact/avatargroup';
+import { Tooltip } from 'primereact/tooltip';
+import { classNames } from 'primereact/utils';
+
+const UI_AVATAR_URL = 'https://ui-avatars.com/api/?background=random&name=';
+
+interface AvatarGroupCrewProps extends AvatarGroupProps {
+  crew?: {
+    avatar_url: string | null;
+    full_name: string;
+  }[];
+}
+
+const AvatarGroupCrew = forwardRef<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
+  AvatarGroupCrewProps
+>(({ crew, ...props }, ref) => {
+  const uniqueCrewMembers = [
+    ...new Map(crew?.map((item) => [item['full_name'], item])).values(),
+  ];
+
+  const crewSize = uniqueCrewMembers.length;
+  const additionalCrewMemberNames =
+    uniqueCrewMembers
+      .slice(4, 10)
+      .map((item) => item.full_name)
+      .join('\n') +
+    (crewSize > 10 ? `\nés további ${crewSize - 10} ember...` : '');
+
+  return (
+    uniqueCrewMembers && (
+      <AvatarGroup {...ref} {...props}>
+        {uniqueCrewMembers.slice(0, 4).map((item, index) => (
+          <Fragment key={encodeURIComponent(item.full_name) + '-fragment'}>
+            <Tooltip
+              className="text-xs"
+              position="top"
+              target={'.avatarTooltip' + index}
+            />
+            <Avatar
+              className={'avatarTooltip' + index}
+              data-pr-tooltip={item.full_name}
+              icon="pi pi-user"
+              image={item.avatar_url || UI_AVATAR_URL + item.full_name}
+              shape="circle"
+            />
+          </Fragment>
+        ))}
+        {crewSize >= 5 && (
+          <Fragment>
+            <Tooltip
+              className="text-xs"
+              position="top"
+              target=".additional-crew-members-avatar"
+            />
+            <Avatar
+              className={classNames(
+                'additional-crew-members-avatar',
+                crewSize - 4 >= 10 ? 'text-xs' : 'text-sm'
+              )}
+              data-pr-tooltip={additionalCrewMemberNames}
+              label={'+' + (crewSize - 4).toString()}
+              shape="circle"
+            />
+          </Fragment>
+        )}
+      </AvatarGroup>
+    )
+  );
+});
+
+AvatarGroupCrew.displayName = 'AvatarGroupCrew';
+
+export default AvatarGroupCrew;
