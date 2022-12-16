4b49e10b7acb66db1fce8a0569b79e7d5d9803c9
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2022-12-16 18:56:27 +0100

    Add dialog for rating creation

---
+++ frontend-admin/src/components/RatingDialog/RatingDialog.tsx
@@ -0,0 +1,268 @@
+import { forwardRef, useEffect, useState } from 'react';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button } from 'primereact/button';
+import { ConfirmPopup, confirmPopup } from 'primereact/confirmpopup';
+import { Dialog, DialogProps } from 'primereact/dialog';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Message } from 'primereact/message';
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { Rating } from 'primereact/rating';
+import { Tag } from 'primereact/tag';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import useMobile from 'hooks/useMobile';
+
+interface RatingDialogProps extends DialogProps {
+  ratingCreatorName?: string;
+  ratingId?: number;
+  videoId: number;
+  videoTitle: string;
+}
+
+interface IRating {
+  rating: number;
+  review: string;
+}
+
+const validationSchema = yup
+  .object({
+    rating: yup.number().min(1).max(5).integer().required(),
+  })
+  .required();
+
+const RatingDialog = forwardRef<React.Ref<HTMLDivElement>, RatingDialogProps>(
+  (
+    {
+      onHide,
+      ratingCreatorName,
+      ratingId: ratingIdParam,
+      videoId,
+      videoTitle,
+      visible,
+      ...props
+    },
+    ref
+  ) => {
+    const isMobile = useMobile();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [ratingId, setRatingId] = useState<number>(0);
+
+    const {
+      control,
+      formState: { isDirty },
+      handleSubmit,
+      reset,
+    } = useForm<IRating>({
+      resolver: yupResolver(validationSchema),
+      shouldFocusError: false,
+    });
+
+    useEffect(() => {
+      if (visible) {
+        setLoading(true);
+        setRatingId(ratingIdParam || 0);
+        const defaultValues: IRating = {
+          rating: 0,
+          review: '',
+        };
+        reset({ ...defaultValues });
+
+        // TODO: Replace with real API call (check for existing rating or load)
+        console.log('API load start');
+        const timer = setTimeout(() => {
+          setLoading(false);
+          if (Math.floor(Math.random() * 3) || ratingId) {
+            if (!ratingId) {
+              setRatingId(456);
+            }
+            defaultValues.rating = 3;
+            defaultValues.review = 'Tetszett';
+          }
+          console.log('API load finished');
+          reset({ ...defaultValues });
+        }, 500);
+
+        return () => {
+          // TODO: Cancel API call on close
+          clearTimeout(timer);
+        };
+      }
+    }, [ratingIdParam, videoId, visible]);
+
+    const onSubmit = (data: IRating) => {
+      setLoading(true);
+      console.log(data.rating + ' - ' + data.review);
+      if (ratingId) {
+        console.log('Updating rating... ' + ratingId);
+      } else {
+        console.log('Creating rating...');
+      }
+      setTimeout(() => {
+        setLoading(false);
+        if (!ratingId) {
+          setRatingId(123);
+        }
+        reset({ ...data });
+      }, 500);
+    };
+
+    const handleDelete = () => {
+      setLoading(true);
+      console.log('Deleting rating...');
+
+      // TODO: Use real API call then close dialog
+      setTimeout(() => {
+        onHide();
+      }, 1000);
+    };
+
+    const onDelete = (
+      event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      confirmPopup({
+        accept: handleDelete,
+        acceptClassName: 'p-button-danger',
+        icon: 'pi pi-exclamation-triangle',
+        message: 'Biztosan törölni akarod az értékelést?',
+        target: event.currentTarget,
+      });
+    };
+
+    const renderFooter = () => {
+      return (
+        <div>
+          <ConfirmPopup />
+          {ratingId > 0 && (
+            <Button
+              className="p-button-text p-button-danger"
+              disabled={loading}
+              icon="pi pi-trash"
+              label="Törlés"
+              onClick={onDelete}
+            />
+          )}
+          <Button
+            className="p-button-text"
+            disabled={loading}
+            icon="pi pi-times"
+            label="Mégsem"
+            onClick={onHide}
+          />
+          <Button
+            autoFocus
+            icon="pi pi-check"
+            label="Mentés"
+            loading={loading}
+            onClick={handleSubmit(onSubmit)}
+          />
+        </div>
+      );
+    };
+
+    const renderHeader = () => {
+      return (
+        <div className="align-items-center flex justify-content-start">
+          <span style={{ whiteSpace: ratingCreatorName ? 'pre' : 'normal' }}>
+            Értékelés{' '}
+          </span>
+          {ratingCreatorName && (
+            <span style={{ color: 'var(--primary-color)' }}>
+              - {ratingCreatorName}
+            </span>
+          )}
+          {loading && (
+            <ProgressSpinner
+              style={{
+                height: '1.25rem',
+                marginLeft: '1rem',
+                width: '1.25rem',
+              }}
+            />
+          )}
+        </div>
+      );
+    };
+
+    return (
+      <Dialog
+        closeOnEscape={!isDirty}
+        footer={renderFooter}
+        header={renderHeader}
+        onHide={onHide}
+        style={{ width: isMobile ? '95vw' : '50vw' }}
+        visible={visible}
+        {...props}
+        {...ref}
+      >
+        <form className="formgrid grid p-fluid">
+          <div className="field col-12">
+            <label
+              className="align-items-center flex font-medium text-900 text-sm"
+              htmlFor="rating"
+            >
+              Hogy tetszett a(z) <Tag className="mx-1">{videoTitle}</Tag> videó?
+            </label>
+            <Controller
+              control={control}
+              name="rating"
+              render={({ field, fieldState }) => (
+                <div className="align-items-center flex justify-content-start">
+                  <Rating
+                    cancel={false}
+                    disabled={loading}
+                    id={field.name}
+                    {...field}
+                  />
+                  {fieldState.error && (
+                    <small
+                      className="block p-error pl-2"
+                      id={field.name + '-help'}
+                    >
+                      Üres értékelés nem adható!
+                    </small>
+                  )}
+                </div>
+              )}
+            />
+          </div>
+          <div className="field col-12">
+            <label className="font-medium text-900 text-sm" htmlFor="rating">
+              Szöveges értékelés
+            </label>
+            <Controller
+              control={control}
+              name="review"
+              render={({ field }) => (
+                <InputTextarea
+                  autoResize
+                  disabled={loading}
+                  id={field.name}
+                  rows={5}
+                  {...field}
+                />
+              )}
+            />
+          </div>
+          {isDirty && (
+            <div className="col-12">
+              <Message
+                className="justify-content-start"
+                severity="warn"
+                text={
+                  ratingId
+                    ? 'A módosításaid még nincsenek elmentve!'
+                    : 'Az értékelésed még nincs elmentve!'
+                }
+              />
+            </div>
+          )}
+        </form>
+      </Dialog>
+    );
+  }
+);
+RatingDialog.displayName = 'RatingDialog';
+
+export default RatingDialog;
--- frontend-admin/src/components/VideosDataTable/VideosDataTable.tsx
+++ frontend-admin/src/components/VideosDataTable/VideosDataTable.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, lazy, Suspense, useState } from 'react';
 
 import { Button } from 'primereact/button';
 import { Column } from 'primereact/column';
@@ -9,6 +9,8 @@ import { useNavigate } from 'react-router-dom';
 import Avatar from 'components/Avatar/Avatar';
 import { VideoStatusTag } from 'components/StatusTag/StatusTag';
 import { UsersDataType } from 'components/UsersDataTable/UsersDataTable';
+
+const RatingDialog = lazy(() => import('components/RatingDialog/RatingDialog'));
 
 export type VideoDataType = {
   avg_rating: number;
@@ -28,6 +30,12 @@ const VideosDataTable = forwardRef<
   VideosDataTableProps
 >(({ requestId, ...props }, ref) => {
   const navigate = useNavigate();
+
+  const [ratingDialogVideoId, setRatingDialogVideoId] = useState<number>(0);
+  const [ratingDialogVideoTitle, setRatingDialogVideoTitle] =
+    useState<string>('');
+  const [ratingDialogVisible, setRatingDialogVisible] =
+    useState<boolean>(false);
 
   const editorBodyTemplate = ({ editor }: VideoDataType) => {
     return (
@@ -60,13 +68,18 @@ const VideosDataTable = forwardRef<
     );
   };
 
-  const actionBodyTemplate = ({ id }: VideoDataType) => {
+  const actionBodyTemplate = ({ id, title }: VideoDataType) => {
     return (
       <div>
         <Button
           aria-label="Értékelés"
           className="mr-2 p-button-info p-button-outlined"
           icon="pi pi-star"
+          onClick={() => {
+            setRatingDialogVideoId(id);
+            setRatingDialogVideoTitle(title);
+            setRatingDialogVisible(true);
+          }}
         />
         <Button
           aria-label="Ugrás a videóhoz"
@@ -80,47 +93,57 @@ const VideosDataTable = forwardRef<
   };
 
   return (
-    <DataTable
-      dataKey="id"
-      emptyMessage="Nem található videó."
-      responsiveLayout="scroll"
-      showGridlines
-      sortField="title"
-      sortOrder={1}
-      stripedRows
-      value={[]}
-      {...props}
-      {...ref}
-    >
-      <Column field="title" header="Videó címe" sortable />
-      <Column
-        align="center"
-        body={statusBodyTemplate}
-        field="status"
-        header="Státusz"
-        sortable
-      />
-      <Column
-        alignHeader="center"
-        body={editorBodyTemplate}
-        field="editor"
-        header="Vágó"
-        sortable
-        sortField="editor.full_name"
-      />
-      <Column
-        alignHeader="center"
-        body={ratingBodyTemplate}
-        field="avg_rating"
-        header="Értékelések"
-        sortable
-      />
-      <Column
-        body={actionBodyTemplate}
-        bodyStyle={{ overflow: 'visible', textAlign: 'center' }}
-        headerStyle={{ textAlign: 'center', width: '9rem' }}
-      />
-    </DataTable>
+    <>
+      <DataTable
+        dataKey="id"
+        emptyMessage="Nem található videó."
+        responsiveLayout="scroll"
+        showGridlines
+        sortField="title"
+        sortOrder={1}
+        stripedRows
+        value={[]}
+        {...props}
+        {...ref}
+      >
+        <Column field="title" header="Videó címe" sortable />
+        <Column
+          align="center"
+          body={statusBodyTemplate}
+          field="status"
+          header="Státusz"
+          sortable
+        />
+        <Column
+          alignHeader="center"
+          body={editorBodyTemplate}
+          field="editor"
+          header="Vágó"
+          sortable
+          sortField="editor.full_name"
+        />
+        <Column
+          alignHeader="center"
+          body={ratingBodyTemplate}
+          field="avg_rating"
+          header="Értékelések"
+          sortable
+        />
+        <Column
+          body={actionBodyTemplate}
+          bodyStyle={{ overflow: 'visible', textAlign: 'center' }}
+          headerStyle={{ textAlign: 'center', width: '9rem' }}
+        />
+      </DataTable>
+      <Suspense>
+        <RatingDialog
+          onHide={() => setRatingDialogVisible(false)}
+          videoId={ratingDialogVideoId}
+          videoTitle={ratingDialogVideoTitle}
+          visible={ratingDialogVisible}
+        />
+      </Suspense>
+    </>
   );
 });
 VideosDataTable.displayName = 'VideosDataTable';
