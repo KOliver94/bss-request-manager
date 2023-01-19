32e3a24df5fe980cd7ca2a00a6225c6a9d5f0c24
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-01-20 00:41:43 +0100

    Update dependencies

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -2,14 +2,14 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '~primereact': path.resolve(__dirname, 'node_modules/primereact'),
