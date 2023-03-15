eaa2951d763650b73561cec558fa548c1495d1f1
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-03-15 02:06:24 +0100

    Add CrewDataTable

---
+++ frontend-admin/src/components/Details/Request/Crew/AddCrewDialog.tsx
@@ -0,0 +1,152 @@
+import { forwardRef, useState } from 'react';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button } from 'primereact/button';
+import { Dialog, DialogProps } from 'primereact/dialog';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import AutoCompleteStaff from 'components/AutoCompleteStaff/AutoCompleteStaff';
+import useMobile from 'hooks/useMobile';
+
+import AutoCompleteCrewPosition from './AutoCompleteCrewPosition';
+
+interface AddCrewDialogProps extends DialogProps {
+  requestId: number;
+}
+
+interface ICrewCreate {
+  member_id: number;
+  position: string;
+}
+
+const validationSchema = yup
+  .object({
+    member_id: yup
+      .number()
+      .required('A stábtag kiválasztása kötelező!')
+      .nullable(),
+    position: yup
+      .string()
+      .required('A pozíció megadása kötelező!')
+      .max(20, 'A pozíció túl hosszú!')
+      .trim(),
+  })
+  .required();
+
+const AddCrewDialog = forwardRef<React.Ref<HTMLDivElement>, AddCrewDialogProps>(
+  ({ onHide, requestId, visible, ...props }, ref) => {
+    const isMobile = useMobile();
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const {
+      control,
+      formState: { errors, isDirty },
+      handleSubmit,
+    } = useForm<ICrewCreate>({
+      resolver: yupResolver(validationSchema),
+      shouldFocusError: false,
+    });
+
+    const getFormErrorMessage = (name: keyof ICrewCreate) => {
+      return errors[name] ? (
+        <small className="p-error">{errors[name]?.message}</small>
+      ) : (
+        <small className="p-error">&nbsp;</small>
+      );
+    };
+
+    const onSubmit = (data: ICrewCreate) => {
+      setLoading(true);
+      console.log('Adding crew member to ' + requestId);
+      console.log(data.member_id + ' - ' + data.position);
+      setTimeout(() => {
+        setLoading(false);
+        onHide();
+      }, 500);
+    };
+
+    const renderFooter = () => {
+      return (
+        <div>
+          <Button
+            className="p-button-text"
+            disabled={loading}
+            icon="pi pi-times"
+            label="Mégsem"
+            onClick={onHide}
+          />
+          <Button
+            autoFocus
+            icon="pi pi-check"
+            label="Mentés"
+            loading={loading}
+            onClick={handleSubmit(onSubmit)}
+          />
+        </div>
+      );
+    };
+
+    return (
+      <Dialog
+        closeOnEscape={!isDirty}
+        footer={renderFooter}
+        header="Új stábtag hozzáadása"
+        onHide={onHide}
+        style={{ width: isMobile ? '95vw' : '50vw' }}
+        visible={visible}
+        {...props}
+        {...ref}
+      >
+        <form className="formgrid grid p-fluid">
+          <div className="field col-12">
+            <label
+              className="align-items-center flex font-medium text-900 text-sm"
+              htmlFor="rating"
+            >
+              Stábtag
+            </label>
+            <Controller
+              control={control}
+              name="member_id"
+              render={({ field }) => (
+                <>
+                  <AutoCompleteStaff
+                    className="w-full"
+                    disabled={loading}
+                    id={field.name}
+                    {...field}
+                  />
+                  {getFormErrorMessage(field.name)}
+                </>
+              )}
+            />
+          </div>
+          <div className="field col-12">
+            <label className="font-medium text-900 text-sm" htmlFor="rating">
+              Pozíció
+            </label>
+            <Controller
+              control={control}
+              name="position"
+              render={({ field }) => (
+                <>
+                  <AutoCompleteCrewPosition
+                    className="w-full"
+                    disabled={loading}
+                    id={field.name}
+                    {...field}
+                  />
+                  {getFormErrorMessage(field.name)}
+                </>
+              )}
+            />
+          </div>
+        </form>
+      </Dialog>
+    );
+  }
+);
+AddCrewDialog.displayName = 'AddCrewDialog';
+
+export default AddCrewDialog;
---
+++ frontend-admin/src/components/Details/Request/Crew/CrewDataTable.tsx
@@ -0,0 +1,199 @@
+import { useState } from 'react';
+
+import { AutoCompleteChangeEvent } from 'primereact/autocomplete';
+import { Button } from 'primereact/button';
+import {
+  Column,
+  ColumnEditorOptions,
+  ColumnSortEvent,
+} from 'primereact/column';
+import { confirmDialog } from 'primereact/confirmdialog';
+import { DataTable, DataTableRowEditCompleteEvent } from 'primereact/datatable';
+import { Ripple } from 'primereact/ripple';
+import { classNames } from 'primereact/utils';
+
+import AutoCompleteStaff from 'components/AutoCompleteStaff/AutoCompleteStaff';
+import User from 'components/User/User';
+import useMobile from 'hooks/useMobile';
+
+import AddCrewDialog from './AddCrewDialog';
+import AutoCompleteCrewPosition from './AutoCompleteCrewPosition';
+
+type CrewDataType = {
+  member: {
+    full_name: string;
+    avatar_url: string;
+  };
+  id: number;
+  position: string;
+};
+
+type CrewDataTableProps = {
+  requestId: number;
+};
+
+const CrewDataTable = ({ requestId }: CrewDataTableProps) => {
+  const [addCrewDialogVisible, setAddCrewDialogVisible] =
+    useState<boolean>(false);
+  const [data, setData] = useState<CrewDataType[]>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const isMobile = useMobile();
+
+  const deleteActionBodyTemplate = ({ id }: CrewDataType) => {
+    return (
+      <button
+        className="p-link p-row-editor-init"
+        disabled={loading}
+        onClick={() => onRowDelete(id)}
+        name="row-delete"
+        type="button"
+      >
+        <span className="pi pi-fw p-row-editor-init-icon pi-trash"></span>
+        <Ripple />
+      </button>
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setLoading(true);
+    console.log('Deleting crew member... ' + id);
+
+    // TODO: Use real API call
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
+  const header = (
+    <div
+      className={classNames(
+        'align-items-center flex flex-wrap',
+        isMobile ? 'justify-content-start' : ' justify-content-end'
+      )}
+    >
+      <Button
+        className={isMobile ? 'w-full' : ''}
+        icon="pi pi-plus"
+        label="Új stábtag"
+        onClick={() => setAddCrewDialogVisible(true)}
+      />
+    </div>
+  );
+
+  const memberBodyTemplate = ({ member }: CrewDataType) => {
+    return <User imageUrl={member.avatar_url} name={member.full_name} />;
+  };
+
+  const memberEditor = (options: ColumnEditorOptions) => {
+    return (
+      <AutoCompleteStaff
+        className="w-full"
+        disabled={loading}
+        onChange={(e: AutoCompleteChangeEvent) =>
+          options.editorCallback?.(e.value)
+        }
+        value={options.value}
+      />
+    );
+  };
+
+  const memberSortFunction = ({ data, order }: ColumnSortEvent) => {
+    if (!order || Math.abs(order) !== 1) {
+      return data;
+    }
+
+    data.sort((a: CrewDataType, b: CrewDataType) => {
+      const _a = a.member.full_name;
+      const _b = b.member.full_name;
+      return order === -1
+        ? _b.localeCompare(_a, 'hu')
+        : _a.localeCompare(_b, 'hu');
+    });
+
+    return data;
+  };
+
+  const onRowDelete = (id: number) => {
+    confirmDialog({
+      accept: () => handleDelete(id),
+      acceptClassName: 'p-button-danger',
+      header: 'Biztosan törölni akarod a stábtagot?',
+      icon: 'pi pi-exclamation-triangle',
+      message: 'A stábtag visszavonhatatlanul törlés fog kerülni!',
+      style: { width: isMobile ? '95vw' : '50vw' },
+    });
+  };
+
+  const onRowEditComplete = (e: DataTableRowEditCompleteEvent) => {
+    const _data = [...data];
+    const { newData, index } = e;
+
+    _data[index] = newData;
+
+    setData(_data);
+    // TODO: Add API call
+  };
+
+  const positionEditor = (options: ColumnEditorOptions) => {
+    return (
+      <AutoCompleteCrewPosition
+        className="w-full"
+        disabled={loading}
+        onChange={(e: AutoCompleteChangeEvent) =>
+          options.editorCallback?.(e.value)
+        }
+        value={options.value}
+      />
+    );
+  };
+
+  return (
+    <>
+      <DataTable
+        dataKey="id"
+        editMode="row"
+        header={header}
+        onRowEditComplete={onRowEditComplete}
+        showGridlines
+        sortField="member"
+        sortOrder={1}
+        stripedRows
+        value={data}
+      >
+        <Column
+          body={memberBodyTemplate}
+          editor={(options) => memberEditor(options)}
+          header="Név"
+          field="member"
+          sortable
+          sortFunction={memberSortFunction}
+          style={{ minWidth: '15rem', width: '40%' }}
+        />
+        <Column
+          editor={(options) => positionEditor(options)}
+          field="position"
+          header="Pozíció"
+          sortable
+          style={{ minWidth: '15rem', width: '40%' }}
+        />
+        <Column
+          bodyStyle={{ textAlign: 'center' }}
+          headerStyle={{ minWidth: '8rem', width: '10%' }}
+          rowEditor
+        />
+        <Column
+          body={deleteActionBodyTemplate}
+          bodyStyle={{ textAlign: 'center' }}
+          headerStyle={{ minWidth: '4rem', width: '5%' }}
+        />
+      </DataTable>
+      <AddCrewDialog
+        onHide={() => setAddCrewDialogVisible(false)}
+        requestId={requestId}
+        visible={addCrewDialogVisible}
+      />
+    </>
+  );
+};
+
+export default CrewDataTable;
