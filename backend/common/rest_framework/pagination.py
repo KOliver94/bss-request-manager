2d70fdc1f9785e745acfd5220dbc756c19e1f966
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-04-10 02:56:55 +0200

    Option to disable pagination

--- backend/common/rest_framework/pagination.py
+++ backend/common/rest_framework/pagination.py
@@ -1,9 +1,21 @@
+from decouple import strtobool
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 
 class ExtendedPagination(PageNumberPagination):
     page_size = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        """
+        Controls pagination enable/disable option using query param "pagination".
+        If pagination=false is passed in query params, data is returned without pagination.
+        """
+        pagination = request.query_params.get("pagination", "true")
+        if not strtobool(pagination):
+            return None
+
+        return super().paginate_queryset(queryset, request, view)
 
     def get_paginated_response(self, data):
         return Response(
