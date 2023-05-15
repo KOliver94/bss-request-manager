389df6d2e244c4866effc4090da0ccefe6fa8c0d
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-05-16 00:56:08 +0200

    Remove failed unused attempts

--- backend/api/v1/admin/urls.py
+++ backend/api/v1/admin/urls.py
@@ -3,8 +3,6 @@ from django.urls import include, path
 from api.v1.admin.requests.views import RequestAdminListCreateView
 
 urlpatterns = [
-    path("management/", include("api.v1.admin.management.urls")),
     path("requests", RequestAdminListCreateView.as_view()),
     path("requests/", include("api.v1.admin.requests.urls")),
-    path("statistics/", include("api.v1.admin.statistics.urls")),
 ]
