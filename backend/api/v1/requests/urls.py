ee5ce8990ce97fbd4c1fdecdd46425efa8b44348
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-07-23 02:59:31 +0200

    Move and rename RatingRouter

--- backend/api/v1/requests/urls.py
+++ backend/api/v1/requests/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import SimpleRouter
 from api.v1.requests.comments.views import CommentViewSet
 from api.v1.requests.ratings.views import RatingViewSet
 from api.v1.requests.requests.views import RequestViewSet
-from api.v1.requests.routers import RatingRouter
 from api.v1.requests.videos.views import VideoViewSet
+from common.rest_framework.routers import DetailOnlyRouter
 
 router = SimpleRouter(trailing_slash=False)
 router.register(r"requests", RequestViewSet, basename="request")
@@ -14,7 +14,7 @@ request_router = SimpleRouter(trailing_slash=False)
 request_router.register(r"comments", CommentViewSet, basename="comment")
 request_router.register(r"videos", VideoViewSet, basename="video")
 
-video_router = RatingRouter(trailing_slash=False)
+video_router = DetailOnlyRouter(trailing_slash=False)
 video_router.register(r"rating", RatingViewSet, basename="rating")
 
 request_urlpatterns = [
