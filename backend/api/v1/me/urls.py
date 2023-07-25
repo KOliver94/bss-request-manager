32142f56f0230b3fbb81f8853fb246e77ad9924a
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-07-25 02:40:40 +0200

    Add social connect/disconnect view

--- backend/api/v1/me/urls.py
+++ backend/api/v1/me/urls.py
@@ -1,6 +1,16 @@
-from api.v1.me.views import MeViewSet
+from django.urls import include, path
+
+from api.v1.me.views import MeViewSet, OAuth2ConnectDisconnectView
 from common.rest_framework.routers import DetailOnlyRouter
 
 router = DetailOnlyRouter(trailing_slash=False)
 router.register(r"me", MeViewSet, basename="me")
-urlpatterns = router.urls
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "me/social/<str:provider>",
+        OAuth2ConnectDisconnectView.as_view(),
+        name="me:social_connect_disconnect",
+    ),
+]
