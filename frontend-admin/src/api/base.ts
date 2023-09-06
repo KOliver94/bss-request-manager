0ac08bc1cb735194b1098fb09e21f4f89e9594cb
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-09-06 02:40:42 +0200

    Add servers to schema

--- frontend-admin/src/api/base.ts
+++ frontend-admin/src/api/base.ts
@@ -18,7 +18,7 @@ import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, AxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export const BASE_PATH = 'http://localhost'.replace(/\/+$/, '');
+export const BASE_PATH = 'http://localhost:8000'.replace(/\/+$/, '');
 
 /**
  *
