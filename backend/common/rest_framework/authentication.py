a3b5cad6fda78a3cb5cba08e7d343b0d06ca977d
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-09-07 02:46:11 +0200

    Refactor

--- backend/common/rest_framework/authentication.py
+++ backend/common/rest_framework/authentication.py
@@ -1,5 +1,2 @@
 def default_user_authentication_rule(user):
-    if user and user.is_active and not hasattr(user, "ban"):
-        return True
-    else:
-        return False
+    return bool(user and user.is_active and not hasattr(user, "ban"))
