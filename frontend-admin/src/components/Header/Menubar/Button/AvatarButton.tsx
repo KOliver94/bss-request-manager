2924281a96391bfaba9709bc4cbc6b088316b3b3
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-09-18 00:39:56 +0200

    Extend LocalStorageHelper + Remove unnecessary variable assignment

--- frontend-admin/src/components/Header/Menubar/Button/AvatarButton.tsx
+++ frontend-admin/src/components/Header/Menubar/Button/AvatarButton.tsx
@@ -4,21 +4,17 @@ import Avatar from 'components/Avatar/Avatar';
 import { getAvatar, getGroups, getName } from 'helpers/LocalStorageHelper';
 
 const AvatarButton = () => {
-  const fullName = getName();
-  const groupName = getGroups();
-  const imgUrl = getAvatar();
-
   return (
     <li className="border-top-1 lg:border-top-none surface-border">
       <a
         className="align-items-center border-left-2 border-transparent cursor-pointer flex font-medium h-full hover:border-primary lg:border-bottom-2 lg:border-left-none lg:px-3 lg:py-2 p-3 no-underline p-ripple px-6 transition-colors transition-duration-150"
         href="/profile"
       >
-        <Avatar className="lg:mr-0 mr-3" image={imgUrl} />
+        <Avatar className="lg:mr-0 mr-3" image={getAvatar()} />
         <div className="block lg:hidden">
-          <div className="font-medium text-900">{fullName}</div>
+          <div className="font-medium text-900">{getName()}</div>
           <span className="font-medium text-600 text-sm">
-            {groupName.join(', ')}
+            {getGroups().join(', ')}
           </span>
         </div>
         <Ripple />
