4b76f7cdc857f14ab555b3e9cf617e8ac1778f47
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-10-28 02:04:47 +0200

    Use real API calls in CommentCards

--- frontend-admin/src/api/mutations.ts
+++ frontend-admin/src/api/mutations.ts
@@ -1,8 +1,28 @@
 import { adminApi } from './http';
 import {
+  CommentAdminCreateUpdateRequest,
   PatchedRequestAdminUpdateRequest,
   RequestAdminCreateRequest,
 } from './models';
+
+export const requestCommentCreateMutation = (requestId: number) => ({
+  mutationFn: (newComment: CommentAdminCreateUpdateRequest) => {
+    return adminApi.adminRequestsCommentsCreate(requestId, newComment);
+  },
+});
+
+export const requestCommentUpdateMutation = (
+  requestId: number,
+  commentId: number,
+) => ({
+  mutationFn: (updateComment: CommentAdminCreateUpdateRequest) => {
+    return adminApi.adminRequestsCommentsPartialUpdate(
+      commentId,
+      requestId,
+      updateComment,
+    );
+  },
+});
 
 export const requestCreateMutation = () => ({
   mutationFn: (newRequest: RequestAdminCreateRequest) => {
