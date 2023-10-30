eb83ab074b304b52839f832317ea4dadd1a5739d
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-10-30 01:32:29 +0100

    Add new Video button to VideosListPage

--- frontend-admin/src/pages/VideosListPage.tsx
+++ frontend-admin/src/pages/VideosListPage.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { classNames } from 'primereact/utils';
 import { useParams } from 'react-router-dom';
 
 import { requestRetrieveQuery, requestVideosListQuery } from 'api/queries';
+import LinkButton from 'components/LinkButton/LinkButton';
 import VideosDataTable from 'components/VideosDataTable/VideosDataTable';
+import useMobile from 'hooks/useMobile';
 import { queryClient } from 'router';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -17,6 +20,28 @@ export async function loader({ params }: any) {
 const VideosListPage = () => {
   const { requestId } = useParams();
   const { data } = useQuery(requestRetrieveQuery(Number(requestId)));
+  const isMobile = useMobile();
+
+  const videoDataHeader = (
+    <div
+      className={classNames(
+        'align-items-center flex flex-wrap',
+        isMobile ? 'justify-content-start' : ' justify-content-end',
+      )}
+    >
+      <LinkButton
+        buttonProps={{
+          className: isMobile ? 'w-full' : '',
+          icon: 'pi pi-plus',
+          label: 'Új videó',
+        }}
+        linkProps={{
+          className: isMobile ? 'w-full' : '',
+          to: `/requests/${data.id}/videos/new`,
+        }}
+      />
+    </div>
+  );
 
   return (
     <div className="p-3 sm:p-5 surface-ground">
@@ -24,7 +49,10 @@ const VideosListPage = () => {
         {data?.title} - Videók
       </div>
       <div className="border-round p-3 shadow-2 sm:p-4 surface-card">
-        <VideosDataTable requestId={Number(requestId)} />
+        <VideosDataTable
+          header={videoDataHeader}
+          requestId={Number(requestId)}
+        />
       </div>
     </div>
   );
