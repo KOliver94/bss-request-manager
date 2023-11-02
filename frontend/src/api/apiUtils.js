b3b0ca2b612a4be6b33a20cdcd42c0886bfb9c40
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-11-03 00:13:42 +0100

    No timeout

--- frontend/src/api/apiUtils.js
+++ frontend/src/api/apiUtils.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/v1/`,
-  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
