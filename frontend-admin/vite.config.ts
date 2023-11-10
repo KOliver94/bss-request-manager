7632b1bdc95795c08929ee7a8eb7e87279d94d9a
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-11-10 02:50:49 +0100

    Revert "Set base path for frontend-admin"
    This reverts commit c149556e9a303b23c25a0bfd86f7e184c0a18c60.

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -6,7 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admin',
   build: {
     assetsDir: 'static/frontend-admin',
     outDir: 'build',
