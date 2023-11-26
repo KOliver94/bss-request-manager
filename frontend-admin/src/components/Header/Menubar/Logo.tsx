4cce2e82be3a3f41b3afb9554c1f3eaafbd2113e
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-11-26 22:38:04 +0100

    Specify width explicitly to eliminate Lighthouse issue

--- frontend-admin/src/components/Header/Menubar/Logo.tsx
+++ frontend-admin/src/components/Header/Menubar/Logo.tsx
@@ -5,8 +5,9 @@ const Logo = () => {
     <img
       alt="Budavári Schönherz Stúdió"
       className="align-self-center lg:mr-4 mr-0"
-      height="40"
+      height="40px"
       src={logoUrl}
+      width="134px"
     />
   );
 };
