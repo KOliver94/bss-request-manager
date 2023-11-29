e8302c5e089890c5a2c788c15952b699c463fbd5
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-11-29 03:23:16 +0100

    Move AuthenticationProvider inside Layout to provide continous check of token validity

--- frontend-admin/src/Layout.tsx
+++ frontend-admin/src/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet, ScrollRestoration, useNavigation } from 'react-router-dom';
 
 import Header from 'components/Header/Header';
 import LoadingPage from 'pages/LoadingPage';
+import { AuthenticationProvider } from 'providers/AuthenticationProvider';
 
 type LayoutProps = {
   children?: JSX.Element;
@@ -12,12 +13,18 @@ const Layout = ({ children }: LayoutProps) => {
   const navigation = useNavigation();
 
   return (
-    <div className="flex flex-column min-h-screen surface-ground">
-      <Header />
-      {navigation.state == 'loading' ? <LoadingPage /> : children ?? <Outlet />}
-      <ScrollTop threshold={200} />
-      <ScrollRestoration />
-    </div>
+    <AuthenticationProvider>
+      <div className="flex flex-column min-h-screen surface-ground">
+        <Header />
+        {navigation.state == 'loading' ? (
+          <LoadingPage />
+        ) : (
+          children ?? <Outlet />
+        )}
+        <ScrollTop threshold={200} />
+        <ScrollRestoration />
+      </div>
+    </AuthenticationProvider>
   );
 };
 
