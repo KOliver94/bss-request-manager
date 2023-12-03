5a73f5fb0c650fd4fb979a1505baadb83c559835
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-12-03 04:10:54 +0100

    Fix JWT token refreshing + Parallel requests were not handled and multiple refresh token requests was sent which caused issues as at some point the refresh token was blacklisted + Logout was not handled when the access token expired because even after refresh the old refresh token was tried to be blacklisted which already was because of the refresh

--- frontend-admin/src/api/http.ts
+++ frontend-admin/src/api/http.ts
@@ -1,4 +1,4 @@
-import axios, { isAxiosError } from 'axios';
+import axios, { AxiosResponse, isAxiosError } from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
 import {
@@ -17,6 +17,7 @@ import {
   LogoutApiFactory,
   RequestsApiFactory,
 } from './api';
+import { TokenRefresh } from './models';
 
 // TODO: Create some validation view to check if all local storage elements exists otherwise redirect to login
 
@@ -28,6 +29,19 @@ const axiosInstance = axios.create({
 });
 
 const basePath = import.meta.env.VITE_API_URL;
+let refreshTokenPromise: null | Promise<AxiosResponse<TokenRefresh>>;
+
+async function fetchRefreshToken(refreshToken: string) {
+  const response = await loginApi.loginRefreshCreate({
+    refresh: refreshToken,
+  });
+
+  const accessToken = response.data.access;
+  axiosInstance.defaults.headers.Authorization = `Bearer ${accessToken}`;
+  refreshTokenPromise = null;
+
+  return response;
+}
 
 axiosInstance.interceptors.response.use(
   (response) => response,
@@ -39,47 +53,61 @@ axiosInstance.interceptors.response.use(
     const originalRequest = error.config;
     const refreshToken = getRefreshToken();
 
-    // If requests fail with 401 Unauthorized because JWT token is not valid try to get new token with refresh token.
     if (
       error.response?.status === 401 &&
-      error.response.data?.code === 'token_not_valid' &&
-      !error.config?.url?.includes('/login/refresh') &&
-      originalRequest &&
-      refreshToken &&
-      !isRefreshTokenExpired()
+      error.response.data?.code === 'token_not_valid'
     ) {
-      try {
-        const response = await LoginApiFactory(
-          undefined,
-          basePath,
-        ).loginRefreshCreate({
-          refresh: refreshToken,
-        });
+      if (error.config?.url?.includes('logout') && originalRequest) {
+        // If the access token expired on logout refresh the tokens
+        // which will create a new refresh token so try to blacklist the new refresh token.
+        refreshTokenPromise ??= fetchRefreshToken(refreshToken);
+        const response = await refreshTokenPromise;
+
         const accessToken = response.data.access;
         const newRefreshToken = response.data.refresh;
 
-        setAccessToken(accessToken);
-        setRefreshToken(newRefreshToken);
-        setRefreshTokenExpirationTime(jwtDecode(newRefreshToken).exp);
-
-        axiosInstance.defaults.headers.Authorization = `Bearer ${accessToken}`;
         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-        return await axiosInstance(originalRequest);
-      } catch {
-        // If an error occurs during token refresh log the user out.
-        // Remove tokens and auth header.
+        originalRequest.data = { refresh: newRefreshToken };
+        return axiosInstance(originalRequest);
+      }
+
+      if (error.config?.url?.includes('/login/refresh')) {
+        // This should be the case when the refresh token is not valid or blacklisted.
+        // Redirect the user to the login page.
         axiosInstance.defaults.headers.Authorization = null;
         localStorage.clear();
         setRedirectedFrom(window.location.pathname);
         window.location.href = '/login';
+        return Promise.reject(error);
+      }
+
+      if (originalRequest && refreshToken && !isRefreshTokenExpired()) {
+        // If requests fail with 401 Unauthorized because JWT token is not valid
+        // try to get new token with refresh token.
+        try {
+          refreshTokenPromise ??= fetchRefreshToken(refreshToken);
+          const response = await refreshTokenPromise;
+
+          const accessToken = response.data.access;
+          const newRefreshToken = response.data.refresh;
+
+          setAccessToken(accessToken);
+          setRefreshToken(newRefreshToken);
+          setRefreshTokenExpirationTime(jwtDecode(newRefreshToken).exp);
+
+          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+          return axiosInstance(originalRequest);
+        } catch (err) {
+          return Promise.reject(err);
+        }
       }
     }
-
     return Promise.reject(error);
   },
 );
 
 export const adminApi = AdminApiFactory(undefined, basePath, axiosInstance);
+export const loginApi = LoginApiFactory(undefined, basePath, axiosInstance);
 export const logoutApi = LogoutApiFactory(undefined, basePath, axiosInstance);
 export const requestsApi = RequestsApiFactory(
   undefined,
