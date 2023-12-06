7174701c6f3da3aad403b51d9a1445742fff2371
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-12-06 02:13:41 +0100

    Decrease performance sample rate to avoid exceeding the monthly limit

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -49,7 +49,7 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production,
-    traces_sample_rate=0.25,
+    traces_sample_rate=0.15,
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
