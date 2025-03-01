5375e0220a41f255313f485ae35f5461857ac050
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2025-03-01 02:36:48 +0100

    Upgrade React and ESLint + Migrate to ESLint flat config

---
+++ frontend/eslint.config.cjs
@@ -0,0 +1,79 @@
+const globals = require('globals');
+const eslintPlugin = require('@eslint/js');
+const reactPlugin = require('eslint-plugin-react');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const jsxA11yPlugin = require('eslint-plugin-jsx-a11y');
+const importPlugin = require('eslint-plugin-import');
+const prettierPluginRecommended = require('eslint-plugin-prettier/recommended');
+const prettierConfig = require('eslint-config-prettier');
+const path = require('path');
+const { off } = require('process');
+
+const currentDir = path.resolve(__dirname);
+
+module.exports = [
+  eslintPlugin.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  reactHooksPlugin.configs['recommended-latest'],
+  jsxA11yPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.recommended,
+  prettierPluginRecommended,
+  prettierConfig,
+  { ignores: ['**/eslint.config.cjs'] },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'off',
+    },
+    rules: {
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: ['router/dom'],
+        },
+      ],
+      'jsx-a11y/no-autofocus': 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react/forbid-prop-types': 'off',
+      'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+      'react/jsx-props-no-spreading': 'off',
+      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+      'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx'],
+          paths: [path.resolve(currentDir, 'src')],
+          moduleDirectory: ['node_modules', 'src'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.js', '.jsx'],
+      },
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: '19.0.0',
+      },
+    },
+  },
+];
